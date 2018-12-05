5bc456c4dea5e7be1e53b11b67d1c12a7fa26bc9
tkraupp435 <tkraupp435@gmail.com>
2018-12-05 09:38:32 -0700

    Revert "stuff"
    This reverts commit c20a0cc680872bbb7c8d8f6c6bb0def8a552071a.

--- INTEX/Controllers/HomeController.cs
+++ INTEX/Controllers/HomeController.cs
@@ -4,10 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data;
-using System.Data.Entity;
-using System.Net;
-using INTEX.Models;
 
 namespace INTEX.Controllers
 {
