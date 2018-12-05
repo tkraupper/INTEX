6376b2532379a9e19e67ed5aca869be9ef6d3abe
tkraupp435 <tkraupp435@gmail.com>
2018-12-05 12:17:25 -0700

    new customer form and new link on the layout page

--- INTEX/Controllers/SingaporePortalController.cs
+++ INTEX/Controllers/SingaporePortalController.cs
@@ -1,38 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using INTEX.DAL;
+using INTEX.Models;
 
 namespace INTEX.Controllers
 {
     public class SingaporePortalController : Controller
     {
-        // GET: SingaporeEmployee
+        //VARIABLES
+        private IntexContext db = new IntexContext();
+        public static int currentEmployee = -1;
+
+        // HOME PAGE
         public ActionResult Index()
         {
-            return View();
+            if (currentEmployee == -1)
+            {
+                return RedirectToAction("Login");
+            }
+            EmployeeCredentials employee = db.EmployeeCredential.Find(currentEmployee);
+            return View(employee);
         }
 
-        //LOGIN METHODS FOR THE SINGAPORE EMPLOYEES
+        //LOGIN METHODS FOR THE SEATTLE EMPLOYEES
         [HttpGet]
         public ActionResult Login()
         {
-            return View();
+            return View("Login");
         }
 
         [HttpPost]
         public ActionResult Login(string uname, string psw)
         {
-            if (uname == "Missouri" && psw == "ShowMe")
+            List<EmployeeCredentials> logins = db.EmployeeCredential.ToList();
+            foreach (EmployeeCredentials creds in logins)
             {
-                return RedirectToAction("Index", "SingaporePortal");
+                if (uname == creds.Username && psw == creds.Password)
+                {
+                    currentEmployee = creds.EmployeeID;
+                    return RedirectToAction("Index");
+                }
             }
-            else
-            {
-                return View("Login");
-            }
-
+            return View("Login");
         }
     }
 }
